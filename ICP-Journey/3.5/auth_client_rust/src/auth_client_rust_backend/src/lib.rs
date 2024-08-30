544af58a627f0d5c13da111cb4a6cbfe4eb8799f
7ashraf <mohamedxman859@gmail.com>
2024-08-30 19:06:42 +0300

    3.5

---
+++ ICP-Journey/3.5/auth_client_rust/src/auth_client_rust_backend/src/lib.rs
@@ -0,0 +1,10 @@
+use candid::{ Principal};
+#[ic_cdk::query]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query]
+fn whoami() -> String {
+    ic_cdk::caller().to_string()
+} 
